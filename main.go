77c077c73cdc8bb827028e4089062c4a98ac8099
Hekmon <edouardhur@gmail.com>
2021-04-11 09:52:39 +0200

    rework main

--- main.go
+++ main.go
@@ -14,14 +14,17 @@ import (
 )
 
 const (
+	// Config env var names
 	apiKeyEnvVarName     = "COINMARKETCAP_APIKEY"
 	BTFSTargetEnvVarName = "BTFS_TARGET"
 	amountEnvVarName     = "TERABYTEMONTH_USD"
-	USDID                = 2781
-	BTTID                = 3718
+	// CMC IDs
+	USDID = 2781
+	BTTID = 3718
 )
 
 var (
+	// Config
 	coinmarketcapAPIKey string
 	btfsTarget          = "http://127.0.0.1:5001"
 	amount              float64
@@ -42,17 +45,16 @@ func main() {
 	} else {
 		fmt.Printf("no custom BTFS API target set thru %s env var, using default: %s\n", BTFSTargetEnvVarName, btfsTarget)
 	}
-	if tmp := os.Getenv(amountEnvVarName); tmp == "" {
-		fmt.Printf("%s env var must be set\n", amountEnvVarName)
-		os.Exit(1)
-	} else {
+	if tmp := os.Getenv(amountEnvVarName); tmp != "" {
 		var err error
 		if amount, err = strconv.ParseFloat(tmp, 64); err != nil {
 			fmt.Printf("amount can not be converted to float64: %s\n", err)
 			os.Exit(1)
 		}
+	} else {
+		fmt.Printf("%s env var must be set\n", amountEnvVarName)
+		os.Exit(1)
 	}
-
 	// Get pricing
 	market := coinmarketcap.New(APIKey)
 	quotes, _, err := market.PriceConversion(context.Background(), coinmarketcap.PriceConversionParams{
@@ -66,25 +68,24 @@ func main() {
 	}
 	bttquote, found := quotes.Quote[BTTID]
 	if !found {
-		fmt.Printf("can not find BTT id %d within the %d returned quotes\n", BTTID, len(quotes.Quote))
+		fmt.Printf("can not find BTT id %d within the %d returned quotes:\n%+v\n", BTTID, len(quotes.Quote), quotes.Quote)
 		os.Exit(2)
 	}
 	hostPrice := bttquote.Price / 3
-	fmt.Printf("%f $ is worth of %f BTT at %v\n", amount, bttquote.Price, bttquote.LastUpdated)
-	fmt.Printf("as a user push 3 times the amount of data on the network for redundancy, if we want a user to be able to store 1TB for a month for %f$\n", amount)
-	fmt.Printf("we need to set the host price for 1TB/month at %f BTT, this amount of BTT is equivalent to the value %d for the BTFS API\n", hostPrice, BTFSPriceConvert(hostPrice))
-
+	fmt.Printf("%0.2f USD is worth %f BTT at %v: with the 3x network redundancy, a host price must be %f BTT for a user to store 1TB/month on the network at this price\n",
+		amount, bttquote.Price, bttquote.LastUpdated, hostPrice)
 	// Update host
-	if err = UpdateHostPrice(hostPrice); err != nil {
+	if err = updateHostPrice(hostPrice); err != nil {
 		fmt.Println(err)
 		os.Exit(2)
 	}
 	fmt.Println("host pricing updated")
 }
 
-func UpdateHostPrice(tokens float64) (err error) {
-	url := fmt.Sprintf("%s/api/v1/storage/announce?host-storage-price=%d", btfsTarget, BTFSPriceConvert(tokens))
-	resp, err := http.Post(url, "application/json", nil) // mimiq web UI request
+func updateHostPrice(tokens float64) (err error) {
+	resp, err := http.Post(
+		fmt.Sprintf("%s/api/v1/storage/announce?host-storage-price=%d", btfsTarget, BTFSPriceConvert(tokens)),
+		"application/json", nil) // mimiq web UI request
 	if err != nil {
 		return
 	}
